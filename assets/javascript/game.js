45817f3f4be9ebaf10e8632d7e12820988ee40a5
Austin Tsiaperas <liketexas@gmail.com>
2017-12-17 20:03:24 -0600

    updated with click event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,12 @@ function character(name,attack,counterDMG,health,portrait){
 var neutral = [];
 var enemy = [];
 var player;
-var portraits = {"orc": "../assets/images/Orc.png","elf":"../assets/images/Elf.png",
-"human": "../assets/images/Human.png","cyclops":"../assets/images/Cyclops.png"};
+var portraits = {"orc": "assets/images/Orc.png","elf":"assets/images/Elf.png",
+"human": "assets/images/Human.png","cyclops":"assets/images/Cyclops.png"};
 var names = Object.keys(portraits);
 var characters = [];
+var playerChose;
+var baseAttack;
 
 function newGame(){
 	var randomAttack;
@@ -30,9 +32,59 @@ function newGame(){
 		randomHealth = 40-randomAttack-randomDMG;
 		//using a total # of points hopefully prevents overpowered characters
 		characters[i] = new character(names[i],randomAttack,randomDMG,randomHealth,portraits[names[i]]);
+		neutral.push(characters[i]);
+		playerChose = false;
     }
-
-    console.log(characters);
+    draw();
+}
+function createHTML(character){
+    var result = "<div class='col character' id='"+character.name+"'>";
+    result+="<h1>"+character.name+"</h1>";
+    result+="<img src='"+character.portrait+"'>";
+    return result;
 }
 
-newGame();
+function draw(){
+    $("#enemies").empty();
+    $("#neutral").empty();
+    $("#player").empty();
+    for (i = 0; i<enemies.length; i++)
+	{
+		$("#enemies").append(createHTML(enemies[i]));
+	}
+
+	for (i = 0; i<neutral.length; i++)
+	{
+		$("#neutral").append(createHTML(neutral[i]));
+	}
+    if (playerChose){
+	$("#player").html(createHTML(player));
+    }
+}	
+	
+
+$(document).ready(function(){
+    newGame();
+    $(".character").on("click", function(){
+        var selected = characters.indexOf(this.id);
+        if (playerChose)
+        {
+
+        }
+        else
+        {
+        	
+        	player = characters[selected];
+        	playerChose = true;
+        	for (i = 0; i<neutral.length; i++)
+        	{
+        		if (i != selected)
+        		{
+        			enemies.push(characters[i]);
+        		}
+            }
+            neutral = [];
+        }
+
+    });
+});
